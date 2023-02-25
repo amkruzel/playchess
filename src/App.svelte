6b2fad4bfe3fc6f5e674ee8d18cdbce94bc4ae7f
Alexander Kruzel <kruzelm.alex@gmail.com>
2023-02-25 13:43:23 -0600

    minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -97,15 +97,9 @@
 </script>
 
 <svelte:head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Play Chess</title>
-  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>♟</text></svg>">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
   <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Playfair+Display:ital,wght@0,500;0,600;0,700;0,800;0,900;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <title>Play Chess</title>
 </svelte:head>
 
 <Header />
